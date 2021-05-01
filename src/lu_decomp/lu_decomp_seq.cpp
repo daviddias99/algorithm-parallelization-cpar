7eeab950872f5b1a305e6bae0e8db971208223b5
daviddias99 <up201705373@fe.up.pt>
2021-05-01 16:05:29 +0100

    Clean up LU decomp sequential

--- src/lu_decomp/lu_decomp_seq.cpp
+++ src/lu_decomp/lu_decomp_seq.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
-
+#include <chrono>
 #include <iomanip>
 #include <iostream>
 
@@ -18,23 +17,35 @@ void printMatrix(double* matrix, size_t size) {
   }
 }
 
-// Falta extrair a L e a U do resultado
-void seqLUClass(double* opMatrix, double* resMatrix, size_t size) {
-  size_t k = 0;
-
-  memcpy(resMatrix, opMatrix, size * size * sizeof(double));
-
-  for(size_t k = 0;resMatrix[k * size + k] != 0 && k < size; k++ ) {
-    for (size_t i = k+1; i < size; i++)
-    {
-      resMatrix[i*size + k] /= resMatrix[k * size + k]; 
+void extractLU(double* op, double* l, double* u, size_t size) {
+  for (size_t i = 0; i < size; i++) {
+    for (size_t j = 0; j < size; j++) {
+      if(i == j){
+        l[i * size + j] = 1;
+        u[i * size + j] = op[i * size + j];
+      }
+      else if (i > j) {
+        l[i * size + j] = op[i * size + j];
+        u[i * size + j] = 0;
+      }
+      else {
+        l[i * size + j] = 0;
+        u[i * size + j] = op[i * size + j];
+      }
     }
-    
-    for (size_t i = k+1; i < size; i++)
-    {
-      for (size_t j = k+1; j < size; j++)
-      {
-        resMatrix[i*size + j] -= resMatrix[i * size + k] * resMatrix[k*size + j]; 
+  }
+}
+
+void luSequential(double* opMatrix, double* resMatrix, size_t size) {
+  for (size_t k = 0; resMatrix[k * size + k] != 0 && k < size; k++) {
+    for (size_t i = k + 1; i < size; i++) {
+      resMatrix[i * size + k] /= resMatrix[k * size + k];
+    }
+
+    for (size_t i = k + 1; i < size; i++) {
+      for (size_t j = k + 1; j < size; j++) {
+        resMatrix[i * size + j] -=
+            resMatrix[i * size + k] * resMatrix[k * size + j];
       }
     }
   }
@@ -55,57 +66,44 @@ int main(int argc, char* argv[]) {
   int matrixSize = atoi(argv[1]);
   int runs = atoi(argv[2]);
   int op = atoi(argv[3]);
-  int blockSize = argc == 5 ? atoi(argv[4]) : matrixSize * matrixSize;
+  // int blockSize = argc == 5 ? atoi(argv[4]) : matrixSize * matrixSize;
 
   const int MATRIX_SIZE_BYTES = (matrixSize * matrixSize) * sizeof(double);
 
   // init matrices
   double* opMatrix = (double*)malloc(MATRIX_SIZE_BYTES);
-  double* lMatrix = (double*)malloc(MATRIX_SIZE_BYTES);
-  double* uMatrix = (double*)malloc(MATRIX_SIZE_BYTES);
   double* resMatrix = (double*)malloc(MATRIX_SIZE_BYTES);
 
   for (int i = 0; i < matrixSize; i++) {
     for (int j = 0; j < matrixSize; j++) {
-      opMatrix[i * matrixSize + j] = (double)getRandBetween(-5, 5);
-      lMatrix[i * matrixSize + j] = 0;
-      uMatrix[i * matrixSize + j] = 0;
+      // Diagonal can't be zero
+      do {
+        opMatrix[i * matrixSize + j] = (double)getRandBetween(-10, 10);
+      } while(i == j && opMatrix[i * matrixSize + j] == 0);
+
       resMatrix[i * matrixSize + j] = 0;
     }
   }
 
-
   for (int i = 0; i < runs; i++) {
+
+    // We do this here instead of inside the functions to avoid affecting the 
+    // times of execution
+    memcpy(resMatrix, opMatrix, matrixSize * matrixSize * sizeof(double));
+
     // Start counting
-
-    double algorithmTime;
-
+    auto begin = std::chrono::high_resolution_clock::now();
     switch (op) {
       case 1:
-        seqLUClass(opMatrix, resMatrix, matrixSize);
+        luSequential(opMatrix, resMatrix, matrixSize);
         break;
-        // case 2:
-        //   algorithmTime = optimCycle(op1Matrix, op2Matrix, resMatrix,
-        //   matrixSize); break;
-        // case 3:
-        //   algorithmTime = blockCycle(op1Matrix, op2Matrix, resMatrix,
-        //   matrixSize, blockSize); break;
     }
-    cout << "OP" << endl;
-    printMatrix(opMatrix, matrixSize);
-    cout << "L" << endl;
-    printMatrix(lMatrix, matrixSize);
-    cout << "U" << endl;
-    printMatrix(uMatrix, matrixSize);
-    cout << "LU" << endl;
-    printMatrix(resMatrix, matrixSize);
+    auto end = chrono::high_resolution_clock::now();
+    auto elapsed = chrono::duration_cast<chrono::microseconds>(end - begin);
 
-    // memset(resMatrix, 0, MATRIX_SIZE_BYTES);
-
-    // cout << algorithmTime << endl;
+    cout << op << " " << matrixSize << " " << elapsed.count()/ 1000000.0 << endl;
   }
 
   free(opMatrix);
-  free(lMatrix);
-  free(uMatrix);
+  free(resMatrix);
 }
